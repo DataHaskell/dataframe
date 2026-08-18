89d7b6d624c40e4aba49b33b5700d3935a633f99
Raghav Sharma <s98raghav@gmail.com>
2026-08-18 23:35:32 +0530

    WIP Parquet Writer (does not compile)

--- dataframe-parquet/src/DataFrame/IO/Parquet/Writer.hs
+++ dataframe-parquet/src/DataFrame/IO/Parquet/Writer.hs
@@ -3,15 +3,113 @@
 
 module DataFrame.IO.Parquet.Writer (writeParquet, writeParquetWithOptions, defaultParquetWriteOptions) where
 
-import DataFrame.Core (DataFrame)
+import qualified Data.Vector as Vector
+import Data.Vector (Vector)
+
+--A Parquet file is a series of row groups followed by the file metadata (which contains the schema and the
+-- metadata for all the rowgroups, which, in turn, contain the metadata for each column chunk). Inside each
+-- rowgroup is a series of column chunks. Column chunks consist of a series of pages. Pages are the PageHeader
+-- followed by RLE encoded definition levels (if they exist), RLE encoded repetition levels (if they exist),
+-- and finally the encoded and then compressed data. I forgot about magic bytes. Those are there too.
+--
+-- For a parquet file to acheive efficient compression we tend to desirc row groupts of a specific size and
+-- for each of our column chunks to have pages that are of a specific size. So we must expose these fiddle
+-- factors to the user so they can tune the writer to have the behavior they want. (there are subtleties to
+-- this that are discussed further below)
+--
 
 data ParquetWriteOptions = ParquetWriteOptions
-    { rowGroupSize :: Int64
+    { pageSize :: Int
+    , rowGroupSize :: Int
+    , batchSize :: Int
+    , preferredCompressionCodec :: CompressionCodec
+    , rowGroupBuffer :: RowGroupBuffer
+    , 
     }
     deriving (Eq, Show)
 
 defaultParquetWriteOptions :: ParquetWriteOptions
 defaultParquetWriteOptions = undefined
+
+-- We'll set the default Page size to 1 MiB and the default rowGroupSize to 128MiB (of course users will be
+-- able to adjust these numbers through write options). We need to hold the entire RowGroup in memory as 
+-- we build is as the ColumnChunks need to be contiguous when written to disk. So we need to hold
+-- buffers for each individual columnChunk as we go row by row and build them; the columnChunks cannot be
+-- interleaved. 
+
+-- Since dataframe is columnar to begin with, we could, in theory, go golumn by column by estimating the size
+-- of a certain slice of a column, but I don't yet see a good way of doing this given we must run the gamut
+-- of encodings and compressions applied to each of those ColumnChunks (and those compression libraries have
+-- their own multifarious strategies for various kinds of data)
+-- 
+-- Each row group has to be a certain size, but each column in a  row group must contain the same number
+-- of rows, even though each column may very well fit the same number of rows in very different amounts
+-- of space. So how do we ensure that we both hit our page size target, our record size target, and have
+-- the same number of rows in each column?
+--
+-- First we must consider the page size and row group sizes to be best effort. They could be slightly above
+-- or below the target. The characteristics of the parquet file will depend on both the write options and
+-- the specific data being encoded. Second, we should run batches of rows through the writer, flushing when
+-- we see that a page has met or exceeded its limit, and when a row group has done the same. So a row group
+-- is flushed specifically only on batch boundaries and we get the same number of rows in every row group
+-- except the last which will be smaller than the rest.
+--
+-- But we should also not overshoot page size egregiously if the user sets a large batch size, so we can
+-- batch the batches (sub-batch) and make it configurable so that page sizes can be tuned if needed. Note:
+-- arrow-rs had something similar but they ran into issues where some columns had really large values.
+-- See https://github.com/apache/arrow-rs/issues/10061. We may need to implement this eventually, but
+-- I'm too lazy to do it right now.
+--
+-- If larger row groups are required (up to a gigabyte in size if not more), we should provide users who
+-- need to minimize memory usage an alternate two pass strategy where we first write to temporary files (one
+-- per columnChunk) until the temporary files have grown to the size of what a rowgroup should actually be
+-- and pipe the temporary files into the output. Essentially our rowgroup buffer is on disk instead of in
+-- memory. This is slower but should use less memory. In cases where there is extra RAM available but the
+-- user chooses the two pass strategy anyway, the temp files will tend to be held in the OS Page Cache (RAM)
+-- anyway.
+--
+-- refer to DataFrame.IO.Utils.RandomAccess for the buffer implementation.
+
+-- I tested write speeds by doing (on Apple Silicon)
+-- `dd if=/dev/zero of=test bs={$n}k oflag=direct conv=fdatasync
+-- Results:
+--
+-- ```
+--    | block size | data (GiB) |  time (s) | GiB/s |
+--    |------------|------------|-----------|-------|
+--    | 4k         |       4.00 |     2.371 |  1.69 |
+--    | 8k         |       4.00 |     1.486 |  2.69 |
+--    | 16k        |       4.00 |     1.045 |  3.83 |
+--    | 32k        |       4.00 |     0.740 |  5.40 |
+--    | 64k        |       4.00 |     0.675 |  5.92 |
+--    | 128k       |       4.00 |     0.669 |  5.98 |
+--    | 256k       |       4.00 |     0.664 |  6.03 |
+--    | 512k       |       4.00 |     0.670 |  5.97 |
+--    | 1024k      |       4.00 |     0.664 |  6.02 |
+--    | 4096k      |       4.00 |     0.668 |  5.99 |
+-- ```
+-- We see that our raw data-writing throughput caps out at 64k and declines slightly above that. So we
+-- need to split our buffer into 64k chunks and flush each to dis k(of course, this
+-- may very well vary from machine to machine, regardless, 64KiB is probably good enough).
+--
+
+
+-- We need writers for each level of the Parquet file
+  
+-- A RowGroupWriter that flushes into the file
+data RowGroupEnv = RowGroupEnv
+  { rowGroupBuffer :: !(Vector ColumnChunkWriter)
+  , pageWriter :: PageWriter 
+  }
+
+type RowGroupWriter b a = ReaderIO (RowGroupEnv b) a
+
+-- The RowGroupWriter has in its env a Vector of ColumnChunkWriters
+-- (which is essentially the row group buffer)
+type ColumnChunkWriter = ReaderIO
+
+-- And the RowGroupWriter also needs a PageWriter
+type PageWriter = ReaderIO
 
 writeParquet :: FilePath -> DataFrame -> IO ()
 writeParquet = writeParquetWithOptions defaultParquetWriteOptions
@@ -49,96 +147,63 @@ initColumnChunkState = undefined
 emptyColumnChunkState :: ColumnChunkStateRecord
 emptyColumnChunkState = undefined
 
-type WriterState a = State WriterStateRecord a
+-- type WriterState a = State WriterStateRecord a
+-- 
+-- generateRowGroup :: DataFrame -> WriterState (Builder, RowGroup)
+-- generateRowGroup dataframe = do
+--   (builder, columns) <- foldChunks columns generateColumnChunk
+--   --TODO big memory if big columns. Use vectors instead
+--   let total_byte_size = sum . map (total_uncompressed_size . cc_meta_data) $ columns
+--       num_rows = fst . dataframeDimensions $ dataframe
+--       total_uncompressed_size = Just $ sum. map (total_uncompressed_size . cc_meta_data) $ columns
+--       rowGroup = undefined --TODO
+--   return (builder, rowGroup)
+-- 
+-- 
+-- generateColumnChunk :: Column -> WriterState (Builder, ColumnChunk)
+-- generateColumnChunk column = do
+--   writerState <- get
+--   put writerState{columnChunkState = initColumnChunkState column}
+--   (builder, _) <- foldChunks (chunkColumn writerState.chunkSize column) generatePage
+--   let file_path = Nothing
+--       file_offset = 0
+--       meta_data = undefined
+--       offset_index_offset = Nothing
+--       offset_index_length = Nothing
+--       column_index_offset = Nothing
+--       column_index_length = Nothing
+--       crypto_metadata     = Nothing
+--       encrypted_column_metadata = Nothing
+--       columnChunk = undefined -- TODO
+--   return (builder, columnChunk)
+-- 
+-- 
+-- generatePage :: Column -> WriterState (Builder, PageHeader) -- PageHeader is also already in the builder
+-- generatePage = undefined
+-- 
+-- generateSchema :: DataFrame -> [SchemaElement]
+-- generateSchema = undefined
+-- 
+-- data Metadata = Metadata 
+--   { schema :: [SchemaElement]
+--   , rowGroups :: [RowGroup]
+--   }
+-- 
+-- buildMetadata :: Metadata -> Builder
+-- buildMetadata = undefined
+-- 
+-- chunkDataFrame :: Int -> DataFrame -> [DataFrame]
+-- chunkDataFrame = undefined
+-- 
+-- chunkColumn :: Int -> Column -> [Column]
+-- chunkColumn = undefined
+-- 
+-- -- TODO IF it becomes a problem, allocate an array ahead of time for storing the metadata
+-- foldChunks :: [chunk] -> (chunk -> WriterState (Builder, metadata)) -> WriterState (Builder, Sequence metadata)
+-- foldChunks chunks process = foldl' f (mempty, mempty) chunks
+--   where
+--     f (builder, metadata) chunk = let (nextBuilder, nextMetadata) = process chunk
+--                                    in (builder <> nextBuilder, metadata <> nextMetadata)
+--   state = State
+--   
 
-generateRowGroup :: DataFrame -> WriterState (Builder, RowGroup)
-generateRowGroup dataframe = do
-  (builder, columns) <- foldChunks columns generateColumnChunk
-  --TODO big memory if big columns. Use vectors instead
-  let total_byte_size = sum . map (total_uncompressed_size . cc_meta_data) $ columns
-      num_rows = fst . dataframeDimensions $ dataframe
-      total_uncompressed_size = Just $ sum. map (total_uncompressed_size . cc_meta_data) $ columns
-      rowGroup = undefined --TODO
-  return (builder, rowGroup)
-
-
-generateColumnChunk :: Column -> WriterState (Builder, ColumnChunk)
-generateColumnChunk column = do
-  writerState <- get
-  put writerState{columnChunkState = initColumnChunkState column}
-  (builder, _) <- foldChunks (chunkColumn writerState.chunkSize column) generatePage
-  let file_path = Nothing
-      file_offset = 0
-      meta_data = undefined
-      offset_index_offset = Nothing
-      offset_index_length = Nothing
-      column_index_offset = Nothing
-      column_index_length = Nothing
-      crypto_metadata     = Nothing
-      encrypted_column_metadata = Nothing
-      columnChunk = undefined -- TODO
-  return (builder, columnChunk)
-
-
-generatePage :: Column -> WriterState (Builder, PageHeader) -- PageHeader is also already in the builder
-generatePage = undefined
-
-generateSchema :: DataFrame -> [SchemaElement]
-generateSchema = undefined
-
-data Metadata = Metadata 
-  { schema :: [SchemaElement]
-  , rowGroups :: [RowGroup]
-  }
-
-buildMetadata :: Metadata -> Builder
-buildMetadata = undefined
-
-chunkDataFrame :: Int -> DataFrame -> [DataFrame]
-chunkDataFrame = undefined
-
-chunkColumn :: Int -> Column -> [Column]
-chunkColumn = undefined
-
--- TODO IF it becomes a problem, allocate an array ahead of time for storing the metadata
-foldChunks :: [chunk] -> (chunk -> WriterState (Builder, metadata)) -> WriterState (Builder, Sequence metadata)
-foldChunks chunks process = foldl' f (mempty, mempty) chunks
-  where
-    f (builder, metadata) chunk = let (nextBuilder, nextMetadata) = process chunk
-                                   in (builder <> nextBuilder, metadata <> nextMetadata)
-
-second :: (b -> c) -> (a, b) -> (a, c)
-second f (a, b) = (a, f b)
-
-newtype State s a = State { runState :: s -> (s, a) }
-
-class Monad m => MonadState s m where 
-  get :: m s
-  get = state $ \s -> (s, s)
-  put :: s -> m ()
-  put s = state $ \_ -> (s, ())
-  state :: (s -> (s, a)) -> m a
-
-instance Functor (State s) where
-  fmap f (State run) = State $ second f . run
-
-instance Applicative (State s) where
-  pure a = State $ (,a)
-  (State r1) <*> (State r2) = State (\s ->
-    let (s', f) = r1 s
-        (s'', x) = r2 s'
-     in (s'', f x)
-  )
-
-instance Monad (State s) where
-  return = pure
-  (State run) >>= f = State $ \s ->
-    let (s', a) = run s
-     in runState (f a) s'
-
-instance MonadState s (State s) where
-  state = State
-  
-
---TODO ColumnIndices and OffsetIndices
--- TODO ColumnOrders
--- dataframe-parquet/src/DataFrame/IO/Utils/RandomAccess.hs
+++ dataframe-parquet/src/DataFrame/IO/Utils/RandomAccess.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module DataFrame.IO.Utils.RandomAccess where
 
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.ByteString (ByteString)
-import Data.ByteString.Internal (ByteString (PS))
+import Data.ByteString.Internal (ByteString (PS), fromForeignPtr0)
+import Data.ByteString.Builder (Builder, byteString)
 import qualified Data.Vector.Storable as VS
 import Data.Word (Word8)
 import DataFrame.IO.Parquet.Seeking (
@@ -16,6 +19,9 @@ import DataFrame.IO.Parquet.Seeking (
 import Foreign (castForeignPtr)
 import System.IO (
     SeekMode (AbsoluteSeek),
+    Handle,
+    WriteMode,
+    withBinaryFile
  )
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
@@ -76,3 +82,102 @@ unsafeToByteString :: VS.Vector Word8 -> ByteString
 unsafeToByteString v = PS (castForeignPtr ptr) offset' len
   where
     (ptr, offset', len) = VS.unsafeToForeignPtr v
+
+-- Writer Buffer -----------------------------------------------------------------
+
+-- Refer to DataFrame.IO.Parquet.Writer for a justification of what we're doing here
+-- There's some overlap here with what's going on in Seeking.hs, so, if this bothers
+-- us, eventually someone will have to come back and reconcile the writer buffer
+-- approach with the reader oriented patterns in Seeking.hs.
+--
+-- We're using MutableByteArrays here for convenience and because we don't need
+-- the more powerful abstractions vector provides (which uses ByteArrays internally)
+--
+-- since we want to use hPutBuf, we're going to need a Ptr, which means are ByteArrya
+-- must be pinned. Now growing pinned arrays can be problematic, but in the vast majority
+-- of cases we shouldn't be growing more than once, if that. See the docs for
+-- Data.Primitive.ByteArray.byteArrayContents.
+
+newtype WritableBinaryHandle = WritableBinaryHandle { unHandle :: Handle }
+
+openWritableBinaryFile :: (HasBuffer m) => FilePath -> m WritableBinaryHandle
+openWritableBinaryFile filepath = liftIO $ do
+  h <- openBinaryFile AppendMode
+  hSetBinaryMode h True
+  hSetBuffering h $ BlockBuffering (Just 65536)
+  pure . WritableBinaryHandle $ h
+
+withWritableBinaryFile :: (HasBuffer m) => FilePath -> (WriteableBinaryHandle -> m r) -> m r
+withWritableBinaryFile filepath action =
+  bracket
+    (openWritableBinaryFile filepath)
+    (hClose . unHandle)
+    action
+
+class (Monad m) => HasBuffer m where
+  type Buffer m
+  askBuffer :: m (Buffer m)
+  residency :: m Int -- number of bytes currently in the buffer
+  writeBytes :: (Foldable f) => f Word8 -> m ()
+  flushTo :: Sink -> m ()
+  
+data BufferPointer = BufferPointer
+  { pointer :: !(IORef (ForeignPtr Word8)) -- Reallocatoble if we need to grow 
+  , size :: !(IORef Int) -- Reallocatable if we need to grow it
+  , cursor :: !(IORef Int)
+  }
+
+data MemoryBuffer = MemoryBuffer
+  { arrayRef :: !(IORef (MutableByteArray RealWorld))
+  , positionRef :: !(IORef Int)
+  }
+
+data Sink = MemorySink MemoryBuffer | FileSink WritableBinaryHandle
+
+instance HasBuffer (ReaderIO MemoryBuffer) where
+  type Buffer (ReaderIO MemoryBuffer) = MemoryBuffer
+
+  askBuffer = ReaderIO id
+  
+  residency = ReaderIO $ \buffer -> readIORef buffer.positionRef
+  
+  writeBytes bytes = ReaderIO $ \buffer -> do
+    position <- readIORef buffer.positionRef 
+    array <- ensureCapacity buffer (position + length bytes)
+    newPosition <- foldM (\i byte -> writeByteArray array i byte >> pure (i + 1)) res bytes
+    writeIORef buffer.positionRef newPosition
+
+  flushTo (MemorySink destination) = ReaderIO $ \source ->
+    guard (destination /= source)
+    sourceArray <- readIORef source.arrayRef
+    sourcePosition <- readIORef source.positionRef
+    destinationPosition <- readIORef destination.positionRef
+    let newDestinationPosition = destinationPosition + sourcePosition
+    destinationArray <- ensureCapacity destination newDestinationPosition
+    copyMutableByteArray
+      destinationArray
+      destinationPosition
+      sourceArray
+      0 -- offset
+      sourcePosition -- number of bytes
+    writeIORef destination.positionRef newDestinationPosition
+    writeIORef source.positionRef 0
+
+  flushTo (FileSink _) = undefined
+
+ensureCapacity :: MemoryBuffer -> Int -> IO MutableByteArray
+ensureCapacity buffer needed = do
+  array <- readIORef buffer.arrayRef
+  if needed <= sizeOfMutableByteArray array
+  then pure array
+  else do
+    grown <- resizeMutableByteArray array (needed + (needed `div` 2))
+    writeIORef buffer.arrayRef grown
+    pure grown
+    
+data BufferHandle = BufferHandle
+  { handle :: !WritableBinaryHandle
+  , cursor :: !(IORef Int)
+  }
+
+
